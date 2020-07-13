ab7564e788601d6236cdc58ccd5d8d5587471f14
Gianpaulo M. Soares <jam_pow@hotmail.com>
2020-07-13 11:01:56 +0000

    feat: added save order button
    #8

--- packages/web-app/src/api/order.js
+++ packages/web-app/src/api/order.js
@@ -1,10 +1,42 @@
+import Joi from '@hapi/joi'
 import axios from './_config'
 import { getAuth } from './common'
+import { simplifyErrorObj } from './common'
 
 const PRIVATE = true
 
+const orderSchema = Joi.object({
+  userId: Joi.number().integer().positive().required(),
+  total: Joi.number().positive().precision(2).required(),
+  //creditUsed: Joi.number.positive().precision(2).required(),
+  //creditEarned: Joi.number.positive().precision(2).required(),
+  items: Joi.array().min(1).items(
+    Joi.object({
+      id: Joi.number().required(),
+      name: Joi.string().required(),
+      code: Joi.string().pattern(/^[0-9]+$/, 'numbers').required(),
+      quantity: Joi.number().integer().positive().min(1).required(),
+      price: Joi.number().positive().precision(2).required(),
+      total: Joi.number().positive().precision(2).required()
+    })
+  )
+})
+
 export const getOrders = async () => {
   const userId = getAuth().sub
-  const resp = await axios(PRIVATE).get(`/users/${userId}/orders`)
-  return resp
+
+  return  await axios(PRIVATE).get(`/users/${userId}/orders`)
+}
+
+export const createOrder = async (_order) => {
+  const userId = getAuth().sub
+
+  const order = { ..._order, userId }
+  const { error, value } = orderSchema.validate(order, { abortEarly: false })
+
+  if (error) {
+    return { error: simplifyErrorObj(error) }
+  }
+
+  return await axios(PRIVATE).post('/orders', value)
 }
--- packages/web-app/src/pages/create-order/component.jsx
+++ packages/web-app/src/pages/create-order/component.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
 import {
+  Button,
   Container,
   Col,
   Form,
   Row,
   Table
 } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
 import SearchBar from './SearchBar'
 import EditModal from './EditItem'
 import currency from '../../common/currency'
+import { createOrder } from '../../api/order'
 
 export default () => {
+  const history = useHistory()
   const [ cartItems, setCartItems ] = useState([])
   const [ editingItem, setEditingItem ] = useState({})
 
@@ -57,6 +61,17 @@ export default () => {
     setEditingItem(item)
   }
 
+  const handleSaveOrder = async () => {
+    const order = {
+      total: cartTotal(),
+      items: cartItems
+    }
+
+    const resp = await createOrder(order)
+
+    history.push('/dashboard')
+  }
+
   return (
     <>
       <Container>
@@ -98,6 +113,11 @@ export default () => {
                 </tfoot>
               </Table>
             </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button onClick={handleSaveOrder}>Salvar</Button>
           </Col>
         </Row>
       </Container>
