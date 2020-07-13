6cc3892972cca3cf7831d7b1d6e0cd10852a4fb0
Gianpaulo M. Soares <jam_pow@hotmail.com>
2020-07-13 23:52:49 +0000

    feat: added orders list in dashboard view
    #9

--- packages/web-app/src/pages/dashboard/component.jsx
+++ packages/web-app/src/pages/dashboard/component.jsx
@@ -3,9 +3,14 @@ import { Link, useLocation } from 'react-router-dom'
 import {
   Alert,
   Button,
+  Col,
+  Container,
+  ListGroup,
+  Row,
   Spinner
 } from 'react-bootstrap'
 import { getOrders } from '../../api/order'
+import currency from '../../common/currency'
 
 export default () => {
   const [ orders, setOrders ] = useState([])
@@ -23,22 +28,51 @@ export default () => {
   }, [])
 
   return (
-    <>
-      <h1>Dashboard</h1>
-      {state && state.success &&
-        <Alert variant="success">
-          Pedido cadastrado
-        </Alert>
-      }
-      {loading 
-        ? <Spinner animation="grow" /> 
-        : orders.map(order => (
-          <div key={order.id}>{order.id}</div>
-        ))
-      }
-      <Button as={Link} to="/create-order">
-        Novo pedido
-      </Button>
-    </>
+    <Container>
+      <Row>
+        <Col>
+
+          <h1>Dashboard</h1>
+
+          {state && state.success &&
+            <Alert variant="success">
+              Pedido cadastrado
+            </Alert>
+          }
+
+          <ListGroup>
+            {loading 
+              ? <Spinner animation="grow" /> 
+              : orders.map(order => (
+                <ListGroup.Item key={order.id}>
+                  <Row>
+                    <Col xs={12}>
+                      <strong>Pedido Nº:</strong> {order.id}
+                    </Col>
+                    <Col xs={12}>
+                      <strong>Data:</strong> {order.createdAt}
+                    </Col>
+                    <Col xs={12}>
+                      <strong>Valor:</strong> {currency(order.total)}
+                    </Col>
+                    <Col xs={12}>
+                      <strong>Cachback:</strong> ({order.cbPercentage || 0}%) {currency(order.cbTotal)}
+                    </Col>
+                    <Col xs={12}>
+                      <strong>Status:</strong> {order.status}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))
+            }
+          </ListGroup>
+
+          <Button as={Link} to="/create-order">
+            Novo pedido
+          </Button>
+        </Col>
+
+      </Row>
+    </Container>
   )
 }
